fc75789d92c94cd6d059bc08f850cc7089c93fcf
puppy9207 <puppy9207@naver.com>
2019-03-04 02:18:26 +0900

    jumbotron & footer

--- koblog/settings.py
+++ koblog/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '73-qrei))mlh1=9d89(wjz*@(4f7@c2hh+lc2)oiwt@jz35c!0'
+#SECRET_KEY = '73-qrei))mlh1=9d89(wjz*@(4f7@c2hh+lc2)oiwt@jz35c!0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '73-qrei))mlh1=9d89(wjz*@(4f7@c2hh+lc2)oiwt@jz35c!0')
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
